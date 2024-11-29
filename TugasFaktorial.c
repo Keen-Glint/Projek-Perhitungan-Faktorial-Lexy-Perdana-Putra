13f5a7d2b3b599bd519dca91a30ef5422ba0c354
Lexy <lexyputra88@gmail.com>
2024-11-29 08:22:40 +0700

    Add files via upload

--- TugasFaktorial.c
+++ TugasFaktorial.c
@@ -7,29 +7,49 @@
 #include <stdio.h>
 
 //fungsi rekursi dengan ternary operator
-int faktorial(int val) {return (val == 0 ? 1 : val * faktorial(val - 1));} 
+long long int faktorial(int val) {return (val == 0 ? 1 : val * faktorial(val - 1));} 
+
+//fungsi untuk menampilkan langkah langkah faktorialnya
+long long int show(long long int num) {
+    if ( num == 0) printf("1");
+    else {
+        for (long long int i = num; i > 0; i--) {
+            if ( num == i) printf("%lld", i);
+            else printf(" * %lld", i);
+        }
+    }  
+}
+
 
 int main(int argc, const char *argv[]) {    
-
-    int num;        //deklarasi variabel
+    long long int num;        //deklarasi variabel
     char pilihan;
         
         //perulangan agar program dapat diulang berkali kali
     do {
-        printf("Masukkan angka yang akan dicari faktorialnya : ");
-        scanf("%d", &num);
+        printf("Masukkan angka yang akan dicari faktorialnya ( 0 - 20 ): ");
 
-        //mengecek apakah angka yang diinput kurang dari 0 atau tidak
-        if (num < 0) printf("\nAngka tidak valid karena kurang dari 0\n");
-        else printf("\nFaktorialnya adalah : %d\n", faktorial(num));
-
+        //mengecek apakah nilai nya bisa dihitung (tidak lebih dari 20!)
+        do {
+            scanf("%lld", &num);
+            //mengecek apakah angka yang diinput valid atau tidak
+            if (num > 22 ) printf("Angka yang dimasukkan tidak valid, masukkan ulang angka yang baru ( 0 - 20 ) : ");
+            else {
+                //menampilkan langkah langkah dan hasil dari faktorialnya
+                printf("\nHasil dari "); 
+                printf("%lld! = ", num);
+                show(num);
+                printf(" = %lld \n", faktorial(num));
+            }
+        } while (num > 22);
+        
         printf("\nIngin mencari faktorial lain? (y/n) : ");
         scanf(" %c", &pilihan);
 
         //mengecek apakah user ingin mengulang program atau tidak
     } while (pilihan == 'y' || pilihan == 'Y');  
 
-    printf("\n\nTerimakasih telah menggunakan program ini");
+    printf("\nTerimakasih telah menggunakan program ini");
 
     return 0;
 }
